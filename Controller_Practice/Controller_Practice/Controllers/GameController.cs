92a420357c46142423b2da066097d6721685462f
Rushikesh Naikwadi <rushee.mailme@gmail.com>
2024-05-04 15:38:05 +0530

    Practice Controller

---
+++ Controller_Practice/Controller_Practice/Controllers/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Controller_Practice.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Name(string name)
+        {
+            return string.Format("<h3>Hi {0},<h3>", name);
+        }
+
+        public string FullName(string fname, string lname)
+        {
+            return string.Format("<h3>Hello {0} {1}</h3>", fname, lname);
+        }
+    }
+}
