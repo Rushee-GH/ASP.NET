4c47af2ae84f7bb0c621d298a349b764fbb58d42
Rushikesh Naikwadi <rushee.mailme@gmail.com>
2024-05-04 16:20:35 +0530

    Views, Search 0f View for ActionMethod, View with different name, Shared

---
+++ V_View/V_View/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace V_View.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Greet()
+        {
+            return View();
+        }
+
+        public ViewResult ViewWithDiffName()
+        {
+            return View("_ViewWithDiffName");
+        }
+    }
+}
